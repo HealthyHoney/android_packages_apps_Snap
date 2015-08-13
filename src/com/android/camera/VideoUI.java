7ed129e6f3b8fa71fff9e3b02c1b9f8fe600fd09
Sanjeev Garg <sangarg@codeaurora.org>
2015-08-13 17:08:39 +0530

    SnapdragonCamera: Fixing NullPointerException in onCreate of VideoModule
    As per new open camera optimization, setContentView is called after init.
    But VideoModule has a dependency in initialization on setContentView as
    if setContentView is not called before using mFilmStripView then its value
    is null. To avoid this changed the location of using mFilmStripView from
    initialization to onResume.
    
    Change-Id: I6f0116fa82554b11b06d733b3952e6596daa50d3

--- src/com/android/camera/VideoUI.java
+++ src/com/android/camera/VideoUI.java
@@ -603,6 +603,9 @@ public class VideoUI implements PieRenderer.PieListener,
             }
         });
 
+    }
+
+    public void setPreviewGesturesVideoUI() {
         mActivity.setPreviewGestures(mGestures);
     }
 
